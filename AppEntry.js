aef1b150baf8d833167483a60166f06d824969fe
Kai Kevin <kevin.c.t.zhang@gmail.com>
2018-06-04 15:13:39 -0700

    reinit project with containers and components

---
+++ AppEntry.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MasterListContainer from './src/containers/MasterListContainer'
+
+export default class AppEntry extends React.Component {
+  render() {
+    return (
+      <MasterListContainer/>
+    )
+  }
+}
+
---
+++ src/containers/MasterListContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import MasterListView from '../components/MasterListView'
+
+export default class MasterListContainer extends React.Component {
+  render() {
+    return (
+      <MasterListView/>
+    )
+  }
+}
+
+
