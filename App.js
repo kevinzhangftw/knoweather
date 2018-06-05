5fc66eddd3ea45fc92313da6e54e6eb77be60718
Kai Kevin <kevin.c.t.zhang@gmail.com>
2018-06-05 09:43:06 -0700

    wrapped rootstack and set up weatherreducer for redux store

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import RootStack from './src/RootStack'
+import { Provider } from 'react-redux'
+import * as redux from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import { config } from 'fetch-redux-crud'
 
+import RootStack from './src/RootStack'
+import reducers from './src/reducers'
+
+
+config.apiUrl = 'http://api.openweathermap.org/data/2.5/find?lat=55.5&lon=37.5&cnt=10'
+
+const store = redux.createStore(
+	reducers,
+	redux.applyMiddleware(thunkMiddleware)
+)
 
 export default class App extends React.Component {
   render() {
     return (
-      <RootStack />
+      <Provider store={store}>
+        <RootStack />
+      </Provider>
     )
   }
 }
